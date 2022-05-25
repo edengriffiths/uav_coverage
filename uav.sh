4080c4bfe8b435702c6b588e64da3c6542e910f2
Eden Griffiths <edengriffy@gmail.com>
2022-05-25 23:58:31 +1000

    Change uav.sh for hyperparameter optimisation

--- uav.sh
+++ uav.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd rl-baselines3-zoo || exit
-python train.py --algo ppo --env uav-v0 --gym-packages uav_gym
+# python train.py --algo ppo --env uav-v0 --gym-packages uav_gym
+python train.py --algo ppo --env uav-v0 --gym-packages uav_gym -n 100000 -optimize --n-trials 1000 --n-jobs 2 --sampler tpe --pruner median
