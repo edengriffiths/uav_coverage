02907ff8a99e4a46db890ac432a49c11c22fc3b6
Eden Griffiths <edengriffy@gmail.com>
2022-05-27 12:05:01 +1000

    Add command for hparams optimisation

--- uav.sh
+++ uav.sh
@@ -2,4 +2,6 @@
 
 cd rl-baselines3-zoo || exit
 # python train.py --algo ppo --env uav-v0 --gym-packages uav_gym
-python train.py --algo ppo --env uav-v0 --gym-packages uav_gym -n 100000 -optimize --n-trials 1000 --n-jobs 2 --sampler tpe --pruner median
+#python train.py --algo ppo --env uav-v0 --gym-packages uav_gym -n 100000 -optimize --n-trials 1000 --n-jobs 2 --sampler tpe --pruner median
+python train.py --algo ppo --env uav-v0 --gym-packages uav_gym -optimize \
+  --study-name test --storage sqlite:///example.db
