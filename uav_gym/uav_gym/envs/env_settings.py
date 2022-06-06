9b73342429de85ef161a1529faf8ed0f78eb2a91
Eden Griffiths <edengriffy@gmail.com>
2022-06-06 17:01:24 +1000

    Change UAV count and add penalties for disconnection and going out of bounds

--- uav_gym/uav_gym/envs/env_settings.py
+++ uav_gym/uav_gym/envs/env_settings.py
@@ -13,21 +13,22 @@ class Settings(object):
 
             # ----
             # USER SETTINGS
-            'NUM_USERS': 15,
-            'NUM_CLUSTERS': 2,
-            'BOUNDARY_FACTOR': 3,
+            'NUM_USERS': 100,
+            'BOUNDARY_FACTOR': 2,
 
             # ----
             # UAV SETTINGS
-            'NUM_UAV': 2,
+            'NUM_UAV': 4,
             'COV_RANGE': 200,  # radius of the coverage range on the ground
-            'COMM_RANGE': 1500,  # distance UAVs can be and still communicate with each other or home base.
+            'COMM_RANGE': 500,  # distance UAVs can be and still communicate with each other or home base.
 
             'INIT_POSITION': (0, 0),
 
             # ----
             # REWARD
-            'P_OUTSIDE_COV': 0.8
+            'P_OUTSIDE_COV': 0.8,
+            'P_DISCONNECT': 1000,
+            'P_OUT_BOUNDS': 1000
         }
 
         self.V['DIST'] = self.V['SCALE']
