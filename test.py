a5592c9940f4ad5f02a38485a26e12967a8cf60b
Eden Griffiths <edengriffy@gmail.com>
2022-06-10 18:17:44 +1000

    Use denormalize_obs function and reducing fps to 10 from 60

--- test.py
+++ test.py
@@ -64,9 +64,9 @@ def get_locs(env, model):
     while not done:
         action, _states = model.predict(obs)
         obs, rewards, done, info = env.step(action)
-        uav_locs.append(obs['uav_locs'])
+        uav_locs.append(env.denormalize_obs(obs)['uav_locs'])
 
-    return obs['user_locs'], np.array(uav_locs)
+    return env.denormalize_obs(obs)['user_locs'], np.array(uav_locs)
 
 
 def mean_cov_score(l_c_scores: np.array([[float]])) -> float:
@@ -191,11 +191,11 @@ def write_data(exp_num, model):
 def make_mp4(exp_num, env, model):
     directory = f"experiments/experiment #{exp_num}"
     user_locs, l_uav_locs = get_locs(env, model)
-    a = animate.AnimatedScatter(user_locs * env.sim_size, l_uav_locs * env.sim_size, cov_range=env.cov_range, comm_range=env.comm_range,
+    a = animate.AnimatedScatter(user_locs, l_uav_locs.tolist(), cov_range=env.cov_range, comm_range=env.comm_range,
                                 sim_size=env.sim_size)
 
     f = rf"{directory}/animation.mp4"
-    writervideo = animation.FFMpegWriter(fps=60)
+    writervideo = animation.FFMpegWriter(fps=10)
     a.ani.save(f, writer=writervideo)
 
 
@@ -211,7 +211,7 @@ if __name__ == '__main__':
     # env.seed(0)
     env.reset()
 
-    model = PPO.load(f"{models_dir}/750000.zip")
+    model = PPO.load(f"{models_dir}/700000.zip")
 
     exp_num = 5
 
