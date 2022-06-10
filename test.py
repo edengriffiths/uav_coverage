4228cec386193b55644956aff28aa6ab36e6401e
Eden Griffiths <edengriffy@gmail.com>
2022-06-10 14:10:02 +1000

    Use multiprocesing to test envs

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import gym
 from typing import Tuple
 
 import uav_gym
-from uav_gym import utils as gym_utils
 import animate
 
 from stable_baselines3 import PPO
@@ -13,19 +12,48 @@ from matplotlib import animation
 import json
 import os
 
+import multiprocessing as multip
 
-def get_c_scores(i, env, model):
-    print(f"trial: {i}")
 
-    obs = env.reset()
-    done = False
-    while not done:
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
+class Environments:
+    gym_environment = 'uav-v0'
 
-    c_scores = env.cov_scores / env.timestep
+    def __init__(self, environments_n, model):
+        self.environments_n = environments_n
+        self.cores_count = multip.cpu_count()
+        self.envs = []
+        self.reset_all_environments()
+        self.model = model
 
-    return c_scores, env.pref_users
+    def __enter__(self):
+        self.pool = multip.Pool(self.cores_count)
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        self.pool.close()
+        self.pool.join()
+
+    def reset_all_environments(self):
+        for env in self.envs:
+            env.close()
+        self.envs = [gym.make(self.gym_environment) for _ in range(self.environments_n)]
+
+    @staticmethod
+    def get_c_scores_single(env):
+        obs = env.reset()
+        done = False
+        while not done:
+            # model comes from global scope so that pickling still works
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = env.step(action)
+
+        c_scores = env.cov_scores / env.timestep
+
+        return c_scores, env.pref_users
+
+    def get_c_scores_all(self):
+        results = self.pool.map(self.get_c_scores_single, self.envs)
+        return results
 
 
 def get_locs(env, model):
@@ -95,11 +123,12 @@ def mean_reg_scores(l_c_scores: np.array([float]), pref_ids: np.array([0 | 1])) 
         ).mean()
 
 
-def get_metrics(env, model, n_trials=100) -> Tuple[float, float, float, float]:
-    l_c_scores, l_pref_ids = map(np.array,
-                                 map(list,
-                                     zip(*[get_c_scores(i, env, model)
-                                           for i in range(n_trials)])))
+def get_metrics(model) -> Tuple[float, float, float, float]:
+    # use multiprocessing to
+    with Environments(multip.cpu_count(), model) as envs:
+        results = envs.get_c_scores_all()
+        l_c_scores, l_pref_ids = np.array(list(zip(*results)))
+
     n_users = len(l_pref_ids[0])
 
     return (
@@ -115,31 +144,35 @@ def conv_locs(locs, sim_size):
     return list(list(loc) for loc in zip(*locs_))
 
 
-def get_data():
+def get_data(model):
     totals = [0, 0, 0, 0]
 
-    means = np.array([0, 0, 0, 0])
     new_means = np.array([1, 1, 1, 1])
     ep = 0.01
 
     i = 0
-    while True:
-        if not any(abs(new_means - means) > ep) and i > 9:
-            break
+    # for the three previous iterations, was the change for all variables smaller than or equal to epsilon?
+    sati = (False, False, False)
+
+    # if the past three iterations satisfied stopping condition, stop.
+    while not all(sati):
         print(f"iteration: {i}")
 
-        totals = list(map(add, totals, get_metrics(env, model, 10)))
+        totals = list(map(add, totals, get_metrics(model)))
         new_means, means = np.array(totals) / (i + 1), new_means
 
-        print(abs(new_means - means))
-        print(new_means)
+        print(f"New metrics: {new_means}")
+        print(f"Change in metrics: {abs(new_means - means)}")
+        print(f"Three prev stopping conditions: {sati}")
+
         i += 1
+        sati = (sati[1], sati[2], not any(abs(new_means - means) > ep))
 
     return new_means
 
 
-def write_data(exp_num):
-    avg_cov_score, avg_fair_ind, avg_pref_score, avg_reg_score = get_data()
+def write_data(exp_num, model):
+    avg_cov_score, avg_fair_ind, avg_pref_score, avg_reg_score = get_data(model)
 
     directory = f"experiments/experiment #{exp_num}"
 
@@ -178,9 +211,9 @@ if __name__ == '__main__':
     # env.seed(0)
     env.reset()
 
-    model = PPO.load(f"{models_dir}/750000.zip", env=env)
+    model = PPO.load(f"{models_dir}/750000.zip")
 
-    exp_num = 4
+    exp_num = 5
 
     directory = f"experiments/experiment #{exp_num}"
 
@@ -194,5 +227,5 @@ if __name__ == '__main__':
     else:
         os.makedirs(directory)
 
-    write_data(exp_num)
+    write_data(exp_num, model)
     make_mp4(exp_num, env, model)
