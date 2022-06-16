78acd51365acd380e394eae2ffbd1d082d320f37
Eden Griffiths <edengriffy@gmail.com>
2022-06-16 16:34:01 +1000

    Add max_timesteps to registration

--- uav_gym/uav_gym/__init__.py
+++ uav_gym/uav_gym/__init__.py
@@ -4,4 +4,5 @@ from gym.envs.registration import register
 register(
     id='uav-v0',
     entry_point='uav_gym.envs:UAVCoverage',
+    max_episode_steps=1800  # this is the maximum flight time of all the UAVs
 )
