f1f25e9598c76b5e2dcbe21b198b7ffd92fd5ee3
Eden Griffiths <edengriffy@gmail.com>
2022-06-25 21:28:44 +1000

    Add unit testing for test.py

--- test/test_test.py
+++ test/test_test.py
@@ -1,1 +1,39 @@
+import unittest
+
 import test
+import numpy as np
+
+class GetData(unittest.TestCase):
+    def test_mean_pref_score(self):
+        """
+        """
+        pref_ids = np.array([1, 0, 0, 1, 0])
+        cov_scores = np.array([0.5, 0.1, 0.3, 0.2, 0.1])
+
+        avg = test.mean_pref_score(cov_scores, pref_ids)
+
+        # edge between home and first uav at [200, 300]
+        self.assertEqual(avg, 0.35)
+
+    def test_mean_reg_score(self):
+        """
+        """
+        pref_ids = np.array([1, 0, 0, 1, 0])
+        cov_scores = np.array([0.5, 0.2, 0.3, 0.2, 0.1])
+
+        avg = test.mean_reg_score(cov_scores, pref_ids)
+
+        self.assertAlmostEqual(avg, 0.2, 2)
+
+    def test_iq_vals(self):
+        ""
+        l = [6, 1, 3, 2, 4, 5]
+
+        iq_vals = test.get_interquartile_vals(l)
+
+        self.assertEqual(iq_vals, [2, 3, 4, 5])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
