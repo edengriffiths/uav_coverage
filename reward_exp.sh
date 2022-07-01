d1363a1e76e1d2e5a4c0ffe845d5c3ffdb42ca4d
Eden Griffiths <edengriffy@gmail.com>
2022-07-01 16:37:34 +1000

    Add other training to reward exp

--- reward_exp.sh
+++ reward_exp.sh
@@ -3,13 +3,24 @@
 weights=( 1 3 9 )
 
 
-for a in "${weights[@]}"; do
-  for b in "${weights[@]}"; do
-    for g in "${weights[@]}"; do
-      if [ $((a % 3)) != 0 ] || [ $((b % 3)) != 0 ] ||  [ $((g % 3)) != 0 ]; then
-        # sbatch_train.script
-        bash sbatch_train.script uav-v0 $a $b $g
-        fi;
-      done
+#for a in "${weights[@]}"; do
+#  for b in "${weights[@]}"; do
+#    for g in "${weights[@]}"; do
+#      if [ $((a % 3)) != 0 ] || [ $((b % 3)) != 0 ] ||  [ $((g % 3)) != 0 ]; then
+#        # sbatch_train.script
+#        bash sbatch_train.script uav-v0 $a $b $g
+#        fi;
+#      done
+#    done
+#  done
+
+
+for b in "${weights[@]}"; do
+  for g in "${weights[@]}"; do
+    if [ $((b % 3)) != 0 ] ||  [ $((g % 3)) != 0 ]; then
+      # sbatch_train.script
+#      bash sbatch_train.script uav-v0 2 $b $g
+       echo uav-v0 2 $b $g
+      fi;
     done
   done
