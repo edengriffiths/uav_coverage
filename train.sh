27b1bc4fb8606d37657a6920225c77708820c78c
Eden Griffiths <edengriffy@gmail.com>
2022-07-01 16:25:35 +1000

    Fix timesteps issue

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd rl-baselines3-zoo || exit
-python train.py --algo ppo --env $env_id --gym-packages uav_gym --env-kwargs alpha:$alpha beta:$beta gamma:$gamma -f ./logs/${alpha}_${beta}_${gamma} -n 4*10**6
+python train.py --algo ppo --env $env_id --gym-packages uav_gym --env-kwargs alpha:$alpha beta:$beta gamma:$gamma -f ./logs/${alpha}_${beta}_${gamma} -n 4000000
